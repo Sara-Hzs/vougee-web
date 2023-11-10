67b846be642afb5400a26b6c08b0c6161ef9e4c4
Sara <sara.h.sarkhanloo@vougee.gmbh>
2023-11-10 14:55:48 +0100

    Improved floating banner!

--- components/Jobs/Jobs.jsx
+++ components/Jobs/Jobs.jsx
@@ -1,7 +1,42 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
+import {jobs_example} from "../../utils/constants";
 
 export const Jobs = () => {
     const [openFrame, setOpenFrame] = useState(4);
+
+    const [jobs, setJobs] = useState({
+        loading: true,
+        error: false,
+        data: null
+    })
+
+    useEffect(() => {
+        // here we fetch
+        // fetch('backend_url')
+        //     .then(res => {
+        //         // check if data is valid
+        //         setJobs({
+        //             loading: false,
+        //             error: false,
+        //             data: res.data
+        //         })
+        //         // if res is not okay
+        //         throw new Error()
+        //     })
+        //     .catch(() => {
+        //         setJobs({
+        //             loading: false,
+        //             error: true,
+        //             data: null
+        //         })
+        //     })
+        setJobs({
+            loading: false,
+            error: false,
+            data: jobs_example
+        })
+    }, [])
+
 
     const sentences = [
         "High five from frame five!",
@@ -30,27 +65,33 @@ export const Jobs = () => {
                 <h2 className="headline-sub">All positions are remote and full time</h2>
             </div>
             <div className="frames-container">
-            {sentences.map((sentence, index) => (
-                <div
-                    key={index}
-                    className={`frame ${openFrame === index ? 'open' : ''}`}
-                    onClick={() => handleFrameClick(index)}
-                >
-                    {openFrame === index ? (
-                        <>
-                            <h1 className="number-title">{String(sentences.length - index).padStart(2, '0')}</h1>
-                            <h2 className="job-title">{jobTitles[index]}</h2>
-                            <p>{sentence}</p>
-                            <button className="apply-btn">Apply Position</button>
-                        </>
-                    ) : (
-                        <>
-                            <span className="vertical-text">{jobTitles[index]}</span>
-                            <div className="frame-number">{String(sentences.length - index).padStart(2, '0')}</div>
-                        </>
-                    )}
-                </div>
-            ))}
+                {
+                    jobs.loading ?
+                        <div>Loading</div> :
+                        !jobs.error ?
+                        sentences.map((sentence, index) => (
+                                <div
+                                    key={index}
+                                    className={`frame ${openFrame === index ? 'open' : ''}`}
+                                    onClick={() => handleFrameClick(index)}
+                                >
+                                    {openFrame === index ? (
+                                        <>
+                                            <h1 className="number-title">{String(sentences.length - index).padStart(2, '0')}</h1>
+                                            <h2 className="job-title">{jobTitles[index]}</h2>
+                                            <p>{sentence}</p>
+                                            <button className="apply-btn">Apply Position</button>
+                                        </>
+                                    ) : (
+                                        <>
+                                            <span className="vertical-text">{jobTitles[index]}</span>
+                                            <div className="frame-number">{String(sentences.length - index).padStart(2, '0')}</div>
+                                        </>
+                                    )}
+                                </div>
+                            )) :
+                            <div>Error</div>
+                }
             </div>
         </div>
     );
---
+++ utils/constants.js
@@ -0,0 +1,42 @@
+export const jobs_example = [
+    {
+        name: 'UX Designer',
+        id: 'jbdsigbg',
+        description: 'obdfgirbgiberigbierbgberbgierbgiberigbierbgiberibgerjgpjrej',
+        requirements: [
+            'odsngog',
+            'osnognog',
+            'osnosfpsaüfüp'
+        ]
+    },
+    {
+        name: 'Data Scientist',
+        id: 'jbdsigbg',
+        description: 'obdfgirbgiberigbierbgberbgierbgiberigbierbgiberibgerjgpjrej',
+        requirements: [
+            'odsngog',
+            'osnognog',
+            'osnosfpsaüfüp'
+        ]
+    },
+    {
+        name: 'React Developer',
+        id: 'jbdsigbg',
+        description: 'obdfgirbgiberigbierbgberbgierbgiberigbierbgiberibgerjgpjrej',
+        requirements: [
+            'odsngog',
+            'osnognog',
+            'osnosfpsaüfüp'
+        ]
+    },
+    {
+        name: 'Web Designer',
+        id: 'jbdsigbg',
+        description: 'obdfgirbgiberigbierbgberbgierbgiberigbierbgiberibgerjgpjrej',
+        requirements: [
+            'odsngog',
+            'osnognog',
+            'osnosfpsaüfüp'
+        ]
+    }
+]
