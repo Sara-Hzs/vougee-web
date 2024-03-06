33f6c398e7a119cb14f93967927bd453e07ccdd1
sara hzs <sara.h.sarkhanloo@vougee.gmbh>
2024-03-06 16:41:16 +0100

    jobs

--- components/Jobs/Jobs.jsx
+++ components/Jobs/Jobs.jsx
@@ -6,22 +6,26 @@ export const Jobs = () => {
     const [jobs, setJobs] = useState([]);
     const [openFrame, setOpenFrame] = useState(jobs.length - 1);
 
-    useEffect(() => {
+    useEffect(async () => {
         const fetchJobs = async () => {
             try {
-                const response = await fetch('http://localhost:1337/jobs');
+
+                const response = await fetch('https://vougee.gmbh/backend/api/jobs');
+                console.log(response)
                 if (!response.ok) {
                     throw new Error(`HTTP error! status: ${response.status}`);
                 }
                 const data = await response.json();
-                setJobs(data);
+                console.log(data.data)
+                setJobs(data.data);
                 setOpenFrame(data.length - 1);
+
             } catch (error) {
                 console.error("There was a problem fetching job data:", error);
             }
         };
 
-        fetchJobs();
+        await fetchJobs();
     }, []);
 
     const handleFrameClick = (frameNumber) => {
@@ -30,7 +34,7 @@ export const Jobs = () => {
 
     const formatNumber = (number) => String(number).padStart(2, '0');
 
-    // Helper function to split semicolon-separated strings into arrays
+    // to split semicolon-separated strings into arrays
     const splitStringBySemicolon = (str) => {
         return str?.split(';').map(s => s.trim()).filter(Boolean) || [];
     };
@@ -79,13 +83,13 @@ export const Jobs = () => {
                                     </div>
                                 </a>
                                 <h1 className="number-title">{formatNumber(jobs.length - index)}</h1>
-                                <h1 className="job-title">{job.name}</h1>
+                                <h1 className="job-title">{job.attributes.Name}</h1>
                                 <p>{job.description}</p>
 
                                 {/* Requirements */}
                                 <h3 className="frame-section-title">Requirements</h3>
                                 <ul>
-                                    {splitStringBySemicolon(job.requirements).map((requirement, rIndex) => (
+                                    {splitStringBySemicolon(job.attributes.Requirements).map((requirement, rIndex) => (
                                         <li key={rIndex}>{requirement}</li>
                                     ))}
                                 </ul>
@@ -93,7 +97,7 @@ export const Jobs = () => {
                                 {/* Scope of Work */}
                                 <h3 className="frame-section-title">Scope Of Work</h3>
                                 <ul>
-                                    {splitStringBySemicolon(job.scope).map((scopeItem, sIndex) => (
+                                    {splitStringBySemicolon(job.attributes.Scope).map((scopeItem, sIndex) => (
                                         <li key={sIndex}>{scopeItem}</li>
                                     ))}
                                 </ul>
@@ -102,7 +106,7 @@ export const Jobs = () => {
                             </div>
                         ) : (
                             <>
-                                <span className="vertical-text">{job.name}</span>
+                                <span className="vertical-text">{job.attributes.Name}</span>
                                 <div className="frame-number">{formatNumber(jobs.length - index)}</div>
                             </>
                         )}
