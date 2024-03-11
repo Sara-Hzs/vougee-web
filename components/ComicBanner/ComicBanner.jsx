e0b5f6dde398bb249bd5184809b4317bb9586ac8
sara hzs <sara.h.sarkhanloo@vougee.gmbh>
2024-03-11 12:51:46 +0100

    Crewbanner & EmpCount improved!

--- components/ComicBanner/ComicBanner.jsx
+++ components/ComicBanner/ComicBanner.jsx
@@ -3,6 +3,7 @@ import React from "react";
 export const ComicBanner = () => {
     return (
         <div className="ComicBanner">
+
             <button className="workBtn">WORKING AT VOUGEE</button>
             <div className="textsWrapper">
 
